90e253af8a2ec76421d81ef9be65760dcc93b383
ivanmarch <ivanumarch@yandex.ru>
2025-02-09 15:49:18 +0300

    Solution for task #3-4

--- 1.students_and_mentor.py
+++ 1.students_and_mentor.py
@@ -1,4 +1,35 @@
-class Student:
+import random
+
+
+class Gradeable:
+    """
+    Parent class for Student and Lector to calculate avarage grade and
+    compare instances.
+    """
+    def average_grade(self):
+        grades_list = sum(self.grades.values(), [])
+        return round(sum(grades_list) / len(grades_list), 2) if grades_list else 0.0
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            raise TypeError(f'Операнд справа должен иметь тип {self.__class__}')
+        return self.average_grade() == other.average_grade()
+
+    def __lt__(self, other):
+        if not isinstance(other, self.__class__):
+            raise TypeError(f'Операнд справа должен иметь тип {self.__class__}')
+        return self.average_grade() < other.average_grade()
+
+    def __gt__(self, other):
+        if not isinstance(other, self.__class__):
+            raise TypeError(f'Операнд справа должен иметь тип {self.__class__}')
+        return self.average_grade() > other.average_grade()
+
+
+class Student(Gradeable):
+    """
+    Student, role - study courses.
+    """
     def __init__(self, name, surname, gender):
         self.name = name
         self.surname = surname
@@ -8,13 +39,26 @@ class Student:
         self.grades = {}
 
     def rate_lect(self, lecturer, course, grade):
-        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+        if (
+            isinstance(lecturer, Lecturer) and
+            course in lecturer.courses_attached and
+            course in self.courses_in_progress
+        ):
             if course in lecturer.grades:
                 lecturer.grades[course].append(grade)
             else:
                 lecturer.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        return (
+            f'Имя: {self.name}\n'
+            f'Фамилия: {self.surname}\n'
+            f'Средняя оценка за домашние задания: {self.average_grade()}\n'
+            f'Курсы в процессе изучения: {", ".join(self.courses_in_progress)}\n'
+            f'Завершенные курсы: {", ".join(self.finished_courses)}'
+        )
 
 
 class Mentor:
@@ -26,8 +70,11 @@ class Mentor:
         self.surname = surname
         self.courses_attached = []
 
+    def __str__(self):
+        return f'Имя: {self.name} \nФамилия: {self.surname}'
 
-class Lecturer(Mentor):
+
+class Lecturer(Mentor, Gradeable):
     """
     Lecturer, role - gives lectures.
     """
@@ -35,13 +82,20 @@ class Lecturer(Mentor):
         super().__init__(name, surname)
         self.grades = {}
 
+    def __str__(self):
+        return super().__str__() + f'\nСредняя оценка за лекции: {self.average_grade()}'
+
 
 class Reviewer(Mentor):
     """
     Reviewer, role - check homeworks.
     """
     def rate_hw(self, student, course, grade):
-        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+        if (
+            isinstance(student, Student) and
+            course in self.courses_attached and
+            course in student.courses_in_progress
+        ):
             if course in student.grades:
                 student.grades[course].append(grade)
             else:
@@ -50,21 +104,85 @@ class Reviewer(Mentor):
             return 'Ошибка'
 
 
-#
-lecturer = Lecturer(name='Rich', surname='Hoster')
-lecturer.courses_attached += ['Python']
+def calc_average_hw_grade(students, course) -> float:
+    total_grades = []
+    for student in students:
+        if course in student.courses_in_progress:
+            total_grades += student.grades[course]
+    return round(sum(total_grades) / len(total_grades), 2) if total_grades else 0.0
 
-#
-student = Student(name='Alex', surname='Bykov', gender='male')
-student.courses_in_progress += ['Python', 'C++']
-student.rate_lect(lecturer=lecturer, course='Java', grade='10')
-student.rate_lect(lecturer=lecturer, course='Python', grade='10')
-student.rate_lect(lecturer=lecturer, course='Python', grade='8')
-print(lecturer.grades['Python'])
 
-#
-reviewer = Reviewer(name='Rost', surname='Lister')
-reviewer.courses_attached += ['Python']
-reviewer.rate_hw(student=student, course='Python', grade='9')
-reviewer.rate_hw(student=student, course='Python', grade='10')
-print(student.grades.get('Python'))
+def calc_average_lecturer_grade(lecturers, course) -> float:
+    total_grades = []
+    for lecturer in lecturers:
+        if course in lecturer.courses_attached:
+            total_grades += lecturer.grades[course]
+    return round(sum(total_grades) / len(total_grades), 2) if total_grades else 0.0
+
+
+# набор данных для иницилазиации объектов
+courses = ['Python', 'Git', 'Java', 'Введение в программирование']
+names = ['Sam', 'Petr', 'Jack', 'Den', 'Rome', 'Michel']
+genders = ['male', 'female']
+surnames = ['Maikov', 'Nosov', 'Reactov', 'Nedrov', 'Mausovich']
+grades = range(1, 11)
+
+# создаём проверяющего
+reviewer = Reviewer(name=random.choice(names), surname=random.choice(surnames))
+reviewer.courses_attached += courses
+print('1. Проверяющий:')
+print(reviewer)
+
+# создаём лекторов
+lecturers = {}
+for i in range(10):
+    lecturers[i] = Lecturer(
+        name=random.choice(names),
+        surname=random.choice(surnames)
+    )
+    lecturers[i].courses_attached += courses
+print('2. Случайный лектор из списка:')
+print(random.choice(list(lecturers.values())))
+
+# создаём студентов
+students = {}
+for i in range(100):
+    students[i] = Student(
+        name=random.choice(names),
+        surname=random.choice(surnames),
+        gender=random.choice(genders)
+    )
+    students[i].finished_courses += [random.choice(courses)]
+    actual_courses = [course for course in courses if course not in students[i].finished_courses]
+    students[i].courses_in_progress += actual_courses
+    # оцениваем студента
+    for course in courses:
+        reviewer.rate_hw(
+            student=students[i],
+            course=course,
+            grade=random.choice(grades)
+        )
+print('3. Случайный студент из списка:')
+print(random.choice(list(students.values())))
+
+# оцениваем лекторов
+for lecturer in lecturers.values():
+    for course in lecturer.courses_attached:
+        for student in students.values():
+            student.rate_lect(
+                lecturer=lecturer,
+                course=course,
+                grade=random.choice(grades)
+            )
+
+course = random.choice(courses)
+print(
+    f'4. Средняя оценка за домашние задания по всем студентам в рамках курса {course}:',
+    calc_average_hw_grade(students=students.values(), course=course)
+)
+
+course = random.choice(courses)
+print(
+    f'5. Cредняя оценка за лекции всех лекторов в рамках {course}:',
+    calc_average_lecturer_grade(lecturers=lecturers.values(), course=course)
+)
